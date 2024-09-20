334761f7fa27889e152a47bb9ea8b3fa8c390134
ulle73 <rydqvist89@hotmail.com>
2024-09-20 15:29:29 +0200

    homescreen start

---
+++ NETFLIX-DUPE/src/Layout/NavBar.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+
+
+function NavBar() {
+ 
+  return (
+    <>
+    
+          <div >
+            <form >
+              <button
+                type="submit"
+              >
+             
+              </button>
+              <input
+                type="text"
+                placeholder="Search Movie Name from here"
+              />
+            </form>
+          </div>
+        
+          <div >
+           
+          </div>
+       
+    </>
+  );
+}
+
+export default NavBar;
---
+++ NETFLIX-DUPE/src/screens/HOME/Recommended.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+// import Titles from "./../Titles";
+// import {
+//   BsBookmarkStarFill,
+//   BsCaretLeftFill,
+//   BsCaretRightFill,
+// } from "react-icons/bs";
+// import { Swiper, SwiperSlide } from "swiper/react";
+// import { Autoplay, Navigation } from "swiper";
+// import { Movies } from "../../Data/MovieData";
+// import { FaHeart } from "react-icons/fa";
+// import { Link } from "react-router-dom";
+// import Rating from "../Stars";
+
+function Recommended() {
+
+
+  return (
+    <div>
+        RECOMMENDED
+     
+    </div>
+  );
+}
+
+export default Recommended;
