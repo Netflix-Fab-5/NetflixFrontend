c8a0e98c42a2f51dac3f4b622870fc97dc1b85a9
ulle73 <145575457+ulle73@users.noreply.github.com>
2024-09-20 15:31:04 +0200

    Home screen (#38)
    * install prettier and added script to .json (#8)
    
    * homescreen start
    
    ---------
    
    Co-authored-by: Andreas <143389315+and-wg@users.noreply.github.com>

---
+++ NETFLIX-DUPE/src/screens/HOME/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Head({ title }) {
+  return (
+    <div>
+      <img/>
+      <div >
+        <h1>
+          {title && title}
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default Head;
