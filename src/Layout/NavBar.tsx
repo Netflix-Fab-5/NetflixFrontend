1eba103a1b69685a858802758eea57c11bccb4e0
hannaholiero <hannahostebo@gmail.com>
2024-09-20 18:00:41 +0200

    Now Eslint & Prettier works
    So the eslint and prettier works, but i had to edit in many files.

--- src/Layout/NavBar.tsx
+++ src/Layout/NavBar.tsx
@@ -1,30 +1,14 @@
-import React from "react";
-import { Link, NavLink } from "react-router-dom";
-
-
 function NavBar() {
- 
   return (
     <>
-    
-          <div >
-            <form >
-              <button
-                type="submit"
-              >
-             
-              </button>
-              <input
-                type="text"
-                placeholder="Search Movie Name from here"
-              />
-            </form>
-          </div>
-        
-          <div >
-           
-          </div>
-       
+      <div>
+        <form>
+          <button type="submit"></button>
+          <input type="text" placeholder="Search Movie Name from here" />
+        </form>
+      </div>
+
+      <div></div>
     </>
   );
 }
