55783405ddceb1a1c43fee49a4b9de63be94b3b8
hannaholiero <hannahostebo@gmail.com>
2024-09-20 17:28:52 +0200

    testing actions
    just wanna see if it works

--- src/screens/HOME/HomeScreen.tsx
+++ src/screens/HOME/HomeScreen.tsx
@@ -1,27 +1,16 @@
-import React from 'react';
-import Trending from './Trending';
-import Recommended from './Recommended';
-import NavBar from '../../Layout/NavBar';
-
-
-
+import Trending from "./Trending";
+import Recommended from "./Recommended";
+import NavBar from "../../Layout/NavBar";
 
 function HomeScreen() {
   return (
-  
-      <div>
-       
-        <NavBar/>
-       {/* inside header we should have login/register/bookmark/searchbar/movies link to category */}
-    
-
-        <Trending />
-       
-        <Recommended />
-        HOMESCREEN
-      </div>
-     
-  
+    <div>
+      <NavBar />
+      {/* inside header we should have login/register/bookmark/searchbar/movies link to category */}
+      <Trending />
+      <Recommended />
+      HOMESCREEN test lägger till ett ord
+    </div>
   );
 }
 
