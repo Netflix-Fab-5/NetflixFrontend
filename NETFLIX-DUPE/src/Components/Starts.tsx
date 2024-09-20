f55d0e8925433a5be90dcbd52c42fc4833402258
ulle73 <145575457+ulle73@users.noreply.github.com>
2024-09-20 14:03:49 +0200

    Create structure (#37)
    * created a shell with main components just to try
    routes
    
    ---------
    
    Co-authored-by: Andreas <143389315+and-wg@users.noreply.github.com>

---
+++ NETFLIX-DUPE/src/Components/Starts.tsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+
+const Starts: React.FC = () => {
+
+  return (
+    <>
+      <div>
+     STARTS
+      </div>
+    </>
+  );
+};
+
+export default Starts ;
