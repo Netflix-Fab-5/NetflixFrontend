47039b3416c26abbee4104f727aedc1683898707
ulle73 <rydqvist89@hotmail.com>
2024-09-23 13:05:51 +0200

    created registerScreen and functionality

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { MyContextProvider } from './Context/Context';
 import HomeScreen from './SCREENS/HomeScreen';
 import FavoriteScreen from './SCREENS/FavoriteScreen';
 import AllMoviesScreen from './SCREENS/AllMoviesScreen';
+import RegisterScreen from "./SCREENS/RegisterScreen";
 
 
 
@@ -15,6 +16,7 @@ function App() {
         <Route path="/" element={<HomeScreen />} />
         <Route path="/favorites" element={<FavoriteScreen />} /> {/* Rutt för favoritsidan */}
         <Route path="/movies" element={<AllMoviesScreen />} />
+        <Route path="/register" element={<RegisterScreen />} />
       </Routes>
     </MyContextProvider>
   );
