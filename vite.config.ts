ce3a6169b14fb9ab29123deebb18bd8e59bc70c7
ulle73 <145575457+ulle73@users.noreply.github.com>
2024-09-24 09:41:09 +0200

    added testing globals to avoid importing everywhere (#52)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    // you might want to disable it, if you don't have tests that rely on CSS
+    // since parsing CSS is slow
+    css: true,
+  },
 });
