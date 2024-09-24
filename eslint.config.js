2ddc30e491c896579bcd024cc73a1c408883f681
hannaholiero <hannahostebo@gmail.com>
2024-09-24 10:29:19 +0200

    reverted to old eslint config
    No longer runs prettier with fix to prevent false positives

--- eslint.config.js
+++ eslint.config.js
@@ -2,23 +2,18 @@ import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
-import tsPlugin from "@typescript-eslint/eslint-plugin";
-import tsParser from "@typescript-eslint/parser";
+import tseslint from "typescript-eslint";
 
-export default [
+export default tseslint.config(
+  { ignores: ["dist"] },
   {
-    ignores: ["dist"],
-  },
-  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
-      parser: tsParser,
       ecmaVersion: 2020,
-      sourceType: "module",
       globals: globals.browser,
     },
     plugins: {
-      "@typescript-eslint": tsPlugin,
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
     },
@@ -28,26 +23,6 @@ export default [
         "warn",
         { allowConstantExport: true },
       ],
-      "@typescript-eslint/no-unused-vars": ["off"],
     },
   },
-  {
-    files: ["**/*.{js,jsx}"],
-    languageOptions: {
-      ecmaVersion: 2020,
-      sourceType: "module",
-      globals: globals.browser, // Globala variabler för webbläsare
-    },
-    plugins: {
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules, // Rekommenderade regler för React Hooks
-      "react-refresh/only-export-components": [
-        "warn",
-        { allowConstantExport: true },
-      ],
-    },
-  },
-];
+);
