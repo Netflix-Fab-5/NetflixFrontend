2d2c79021bddecff4b9913c148ee0f56cc344882
CarolineMagnenat <145575566+CarolineMagnenat@users.noreply.github.com>
2024-09-27 00:32:30 +0200

    styling done

--- src/components/navbar/SearchMovie.tsx
+++ src/components/navbar/SearchMovie.tsx
@@ -39,18 +39,40 @@ function SearchMovie() {
   if (error) return <div>{error}</div>;
 
   return (
-    <div>
-      <form>
+    <div className="relative p-4">
+      <form className="mb-4">
         <input
           type="text"
           placeholder="Search Movie Name from here"
           value={query}
           onChange={handleSearch}
+          className="w-72 mx-auto block px-4 py-2 border rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-600"
         />
       </form>
-      <ul>
+      <ul
+        className="absolute left-0 right-0 mx-auto w-80 bg-white shadow-lg rounded-md divide-y divide-gray-200 z-50"
+        style={{ maxHeight: "425px", overflowY: "auto" }} // Justera för att visa 3,5 filmer
+      >
+        {" "}
         {results.length > 0 &&
-          results.map((movie, index) => <li key={index}>{movie.title}</li>)}
+          results.map((movie) => (
+            <li
+              key={movie.id}
+              className="p-4 hover:bg-gray-100 flex items-center"
+            >
+              <img
+                src={movie.thumbnail}
+                alt={movie.title}
+                className="w-8 h-12 object-cover rounded-md mr-4"
+              />
+              <div>
+                <h3 className="text-lg font-semibold">{movie.title}</h3>
+                <p className="text-sm text-gray-500">
+                  {movie.year} | {movie.rating}
+                </p>
+              </div>
+            </li>
+          ))}
       </ul>
     </div>
   );
