0895e8d15c56786cc0872aed4f8fff024c78f418
hannaholiero <hannahostebo@gmail.com>
2024-09-27 13:50:32 +0200

    Added Logout-button to reset sessionsstorage

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -1,10 +1,24 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Header from "./Header";
 import BookmarkButton from "./BookMarkButton";
 import MoviesButton from "./AllMoviesButton";
 import SearchMovie from "./SearchMovie";
+import { logoutUser } from "../../firebase/firebaseAuth"; // Importera logoutUser-funktionen
+import { useAuth } from "../../hooks/useAuth";
 
 function NavBar() {
+  const { user } = useAuth(); // Hämta användarinformation för att visa logga ut-knappen endast om användaren är inloggad
+  const navigate = useNavigate();
+  const handleLogout = async () => {
+    try {
+      await logoutUser(); // Logga ut från Firebase
+      sessionStorage.clear(); // Rensa sessionStorage
+      navigate("/login"); // Omdirigera till inloggningssidan efter utloggning
+    } catch (error) {
+      console.error("Fel vid utloggning:", error);
+    }
+  };
+
   return (
     <>
       <div>
@@ -13,10 +27,14 @@ function NavBar() {
         <BookmarkButton />
         <MoviesButton />
         <SearchMovie />
-        {/* Lägg till en login-knapp här */}
-        <Link to="/register">
-          <button>Register</button>
-        </Link>
+        {!user && (
+          <Link to="/register">
+            <button>Register</button>
+          </Link>
+        )}
+
+        {/* Visa logga ut-knappen om användaren är inloggad */}
+        {user && <button onClick={handleLogout}>Logga ut</button>}
       </div>
 
       <div>{/* Eventuella andra element */}</div>
