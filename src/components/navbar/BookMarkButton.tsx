a535134eca764e024d1016bfd5e1fc4f5ff0610b
ulle73 <rydqvist89@hotmail.com>
2024-10-02 02:33:43 +0200

    overall styling, start

--- src/components/navbar/BookMarkButton.tsx
+++ src/components/navbar/BookMarkButton.tsx
@@ -9,19 +9,7 @@ const BookmarkButton = () => {
       }}
       aria-label="Gå till favoritfilmer"
     >
-      <button
-        style={{
-          backgroundColor: "#ff0000", // En röd färg för att efterlikna Netflix-stilen
-          border: "none",
-          padding: "10px 20px",
-          color: "white",
-          cursor: "pointer",
-          fontSize: "1rem",
-          borderRadius: "5px",
-        }}
-      >
-        Favorites
-      </button>
+      <button>Favorites</button>
     </Link>
   );
 };
