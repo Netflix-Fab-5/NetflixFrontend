d37909e35e0baf6ca7a03cfb0dee6dc6b8c6ce75
ulle73 <145575457+ulle73@users.noreply.github.com>
2024-10-02 14:30:54 +0200

    button styled on homescreen (#167)

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import "./styles/App.css";
 import "./styles/index.css";
 import "./styles/carousel.css";
 import "./styles/headerAnimation.css";
+import "./styles/navbar.css";
 import { BrowserRouter } from "react-router-dom";
 import { MyContextProvider } from "./constants/context.tsx";
 
