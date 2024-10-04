59be7de1dfab5d103c55a1dce5c0f1fea32eda57
ulle73 <rydqvist89@hotmail.com>
2024-10-05 00:45:34 +0200

    no-scroll in animation fix, also added test

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -6,23 +6,39 @@ function Head() {
   const [noScroll, setNoScroll] = useState(true); // State för att styra scroll
 
   useEffect(function () {
-    // Ställ in no-scroll under de första 5 sekunderna
-    const timer = setTimeout(function () {
-      setNoScroll(false);
-    }, 5000);
+    // Kontrollera om 'netflixEffectRun' finns i localStorage
+    const effectRun = localStorage.getItem("netflixEffectRun");
 
-    // Rensa timer när komponenten avmonteras
-    return function () {
-      clearTimeout(timer);
-    };
+    // Om det inte finns eller är false, sätt no-scroll under de första 5 sekunderna
+    if (effectRun !== "true") {
+      setNoScroll(true);
+
+      // Starta timer för att ta bort no-scroll efter 5 sekunder
+      const timer = setTimeout(function () {
+        setNoScroll(false);
+
+        // Uppdatera localStorage för att indikera att effekten har körts
+        localStorage.setItem("netflixEffectRun", "true");
+      }, 5000);
+
+      // Rensa timer när komponenten avmonteras
+      return function () {
+        clearTimeout(timer);
+      };
+    } else {
+      // Om netflixEffectRun redan är true, se till att no-scroll inte är aktiv
+      setNoScroll(false);
+    }
   }, []);
 
-  // Lägg till eller ta bort no-scroll klassen baserat på noScroll state
-  if (noScroll) {
-    document.body.classList.add("no-scroll");
-  } else {
-    document.body.classList.remove("no-scroll");
-  }
+  useEffect(() => {
+    // Lägg till eller ta bort no-scroll-klassen baserat på noScroll-state
+    if (noScroll) {
+      document.body.classList.add("no-scroll");
+    } else {
+      document.body.classList.remove("no-scroll");
+    }
+  }, [noScroll]);
 
   return (
     <div
