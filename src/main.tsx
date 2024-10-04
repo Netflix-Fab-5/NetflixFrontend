fa01d51da9f0615f34b224155d0b4f6aa657707a
ulle73 <rydqvist89@hotmail.com>
2024-10-04 13:29:55 +0200

    deleted some css files, converted to tailwindCSS

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,10 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles/App.css";
 import "./styles/index.css";
-import "./styles/carousel.css";
+
 import "./styles/headerAnimation.css";
-import "./styles/navbar.css";
 import "./styles/loginForm.css";
-import "./styles/allMoviesScreen.css";
+
 import { BrowserRouter } from "react-router-dom";
 import { MyContextProvider } from "./constants/context";
 
